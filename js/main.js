c0dac9545ac4eba7dc61b5b439ef71aa3d656287
Nouamanex <115805829+Nouamanex@users.noreply.github.com>
2023-12-18 14:51:13 +0100

    Nouamane

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let menuIcon= document.getElementById('menu-icon');
+let menuul = document.querySelector('nav ul');
+
+
+menuIcon.onclick = function(){
+    menuul.style.display='block';
+    
+}
+
+
